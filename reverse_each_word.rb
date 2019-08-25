41c25c994b721f7c64e0227dd71639494b9a45f5
Matt Plichta <bigcatplichta@gmail.com>
2019-08-25 18:28:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # def reverse_each_word(string)
 #   word_array = string.split
 #   new_array = []
@@ -8,9 +9,23 @@
 #   end
 #   new_array.join(" ")
 # end
+=======
+def reverse_each_word(string)
+  word_array = string.split
+  
+  word_array.each do |word| 
+    word.reverse 
+  end
+  word_array.join(" ")
+end
+>>>>>>> f0632d4cebda02a0a5bd3fc47b1d84db61a6e62e
 
 def reverse_each_word(string)
   word_array = string.split
   
+<<<<<<< HEAD
   word_array.collect { |word| word.reverse}.join(" ") 
+=======
+  word_array.collect { |word| word.reverse }
+>>>>>>> f0632d4cebda02a0a5bd3fc47b1d84db61a6e62e
 end
